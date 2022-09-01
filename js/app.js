76e21c56e24a5788843f1b04e001cb3d4b72f5e4
Amreal-Lamanera <pieraccini.francesco@gmail.com>
2022-09-01 15:19:24 +0200

    alternative solution

--- js/app.js
+++ js/app.js
@@ -24,14 +24,33 @@ const app = new Vue({
     //     console.log(this.tenEmails);
 
     // }
-    mounted() {
 
-        for (let i = 0; i < 10; i++) {
+    // soluzione
+    // mounted() {
+
+    //     for (let i = 0; i < 10; i++) {
+    //         axios
+    //             .get('https://flynn.boolean.careers/exercises/api/random/mail')
+    //             .then((res) => {
+    //                 this.tenEmails.push(res.data.response)
+    //             })
+    //     }
+    // }
+
+    // soluzione alternativa
+    methods: {
+        getRandomEmail(arrayEmails) {
             axios
                 .get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((res) => {
-                    this.tenEmails.push(res.data.response)
+                    arrayEmails.push(res.data.response)
                 })
+        }
+    },
+    mounted() {
+
+        for (let i = 0; i < 10; i++) {
+            this.getRandomEmail(this.tenEmails);
         }
     }
 
